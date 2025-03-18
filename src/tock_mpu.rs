d45856fdeaf0bc8873ad876272e90aa93891f0b3
Vivien Rindisbacher <vrindisbacher77@gmail.com>
2025-03-17 17:25:56 -0700

    wip

--- src/tock_mpu.rs
+++ src/tock_mpu.rs
@@ -34,6 +34,26 @@ pub fn closest_power_of_two(mut num: usize) -> usize {
     num
 }
 
+#[flux_rs::sig(fn (app_mem_size: usize, region_size: usize) -> usize[num_enabled_subregions(app_mem_size, region_size)] requires region_size >= 256)]
+fn num_enabled_subregions(initial_app_memory_size: usize, region_size: usize) -> usize {
+    (initial_app_memory_size * 8) / region_size + 1
+}
+
+#[flux_rs::sig(fn (region_size: usize) -> usize[subregion_size(region_size)] requires region_size >= 256)]
+fn subregion_size(region_size: usize) -> usize {
+    region_size / 8
+}
+
+#[flux_rs::sig(fn (start: usize, app_mem_size: usize, region_size: usize) -> usize[subregion_enabled_end(start, app_mem_size, region_size)] requires region_size >= 256)]
+fn subregion_enabled_end(start: usize, initial_app_memory_size: usize, region_size: usize) -> usize {
+    start + num_enabled_subregions(initial_app_memory_size, region_size) * subregion_size(region_size)
+}
+
+#[flux_rs::sig(fn (start: usize, memory_size: usize, initial_kernel_memory_size: usize) -> usize[kernel_break(start, memory_size, initial_kernel_memory_size)] requires initial_kernel_memory_size <= start + memory_size)]
+fn kernel_break(start: usize, memory_size: usize, initial_kernel_memory_size: usize) -> usize {
+    start + memory_size - initial_kernel_memory_size
+}
+
 #[flux_rs::refined_by(subregion_end: int, memory_size: int)]
 struct Pair {
     #[field(usize[subregion_end])]
@@ -49,7 +69,8 @@ struct Pair {
         region_start: usize,
         initial_app_memory_size: usize,
         initial_kernel_memory_size: usize,
-    ) -> Pair {new_end: kernel_break(region_start, new_end.memory_size, initial_kernel_memory_size) >= new_end.subregion_end && po2(new_end.memory_size)}
+    ) -> Pair { new_end: kernel_break(region_start, new_end.memory_size, initial_kernel_memory_size) >= new_end.subregion_end }
+        // && po2(new_end.memory_size)}
     requires 
         initial_app_memory_size > 0 // app mem size > 0
         &&
@@ -58,8 +79,8 @@ struct Pair {
         initial_app_memory_size + initial_kernel_memory_size <= memory_size_po2
         &&
         memory_size_po2 >= 512 
-        &&
-        po2(memory_size_po2)
+        // &&
+        // po2(memory_size_po2)
         &&
         region_size == memory_size_po2 / 2 
         &&
@@ -67,17 +88,20 @@ struct Pair {
 )]
 fn overlap_body(
     mut memory_size_po2: usize,
-    mut region_size: usize,
+    region_size: usize,
     mut region_start: usize,
     initial_app_memory_size: usize,
-    _initial_kernel_memory_size: usize,
+    initial_kernel_memory_size: usize,
 ) -> Pair {
-    memory_size_po2 *= 2;
-    region_size *= 2;
+    let enabled_end = subregion_enabled_end(region_start, initial_app_memory_size, region_size);
+    let kbreak = kernel_break(region_start, memory_size_po2, initial_kernel_memory_size);
+    let overlap = enabled_end - kbreak;
+    memory_size_po2 = closest_power_of_two(overlap + memory_size_po2);
+    let region_size = memory_size_po2 / 2;
     if region_start % region_size != 0 {
         region_start += region_size - (region_start % region_size);
     }
-    let num_enabled_subregions = (initial_app_memory_size * 8) / region_size + 1; 
+    let x = true;
 
-    Pair { subregion_end: region_start + num_enabled_subregions * (region_size / 8), memory_size: memory_size_po2 }
+    Pair { subregion_end: subregion_enabled_end(region_start, initial_app_memory_size, region_size), memory_size: memory_size_po2 }
 }
